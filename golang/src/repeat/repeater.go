66e083302ce3afe6afb4d74387d24cb7ae662673
Paul Priest <josephplpriest@gmail.com>
2023-01-23 07:48:59 -0500

    simple tdd in golang

---
+++ golang/src/repeat/repeater.go
@@ -0,0 +1,9 @@
+package repeat
+
+func Repeat(char string) string {
+	var repeated string
+	for i := 0; i < 5; i++ {
+		repeated += char
+	}
+	return repeated
+}
---
+++ golang/src/repeat/repeater_test.go
@@ -0,0 +1,14 @@
+package repeat
+
+import (
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
